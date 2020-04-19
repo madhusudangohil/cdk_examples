ce27f053e64f1281b78c2d6d28de2e17bf1cc116
madhusudan gohil <maamgohil@hotmail.com>
2020-04-19 13:51:45 -0700

    fixed

--- bin/cdk.js
+++ bin/cdk.js
@@ -2,6 +2,9 @@
 
 const cdk = require('@aws-cdk/core');
 const { CdkStack } = require('../lib/cdk-stack');
+const { ECSStack } = require('../lib/cdk-ecs-stack');
+const envUSA = { account: '699589633627', region: 'us-east-2'};
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+//new CdkStack(app, 'CdkStack');
+new ECSStack(app, 'ECSStack', {env:envUSA});
